84a6aa0fd253f975e03b781a73f1f8b83cb09e2d
Abdul Motin <motin.mmk.jr@gmail.com>
2021-11-23 17:14:33 +0600

    Base Template onw

---
+++ app/controllers/Page.php
@@ -0,0 +1,6 @@
+<?php
+class Page{
+    public function __construct(){
+        echo '<h1>Page Are Loade....</h1>';
+    }
+}
---
+++ app/controllers/Pages.php
@@ -0,0 +1,13 @@
+<?php
+
+class Pages{
+    public function __construct(){
+        echo 'Deafuld';
+    }
+    public function home(){
+        echo 'Home page';
+    }
+    public function about(){
+        echo 'About page';
+    }
+}
