664b862680ae3bdb84add0cecf97a207fe220ecb
Abdul Motin <motin.mmk.jr@gmail.com>
2021-11-24 12:21:50 +0600

    Chang Core.php file copy

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -7,10 +7,15 @@ class Pages extends Controller {
     public function index(){
         // i extended the base controller class and i user parrent class constructor
         // to load the view
-        $this->view('index');
+        //create a data array on the view
+        $data = [
+            'title' => 'welcome to my blog',
+            'description' => 'this is the description'
+        ];
+        $this->view('index', $data);
         
     }
-    public function about($id){
-        echo "About page with id: {$id}";
+    public function about(){
+        $this->view('pages/about');
     }
 }
