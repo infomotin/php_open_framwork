efce8caf88e8d8e6cf14b26b8c1b06e09ed7b5b8
Abdul Motin <motin.mmk.jr@gmail.com>
2021-11-24 16:14:33 +0600

    cleaning MVC

---
+++ app/libraries/controller.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+this is the base controller class
+*/
+
+class Controller
+{
+    //load model 
+    public function model($model)
+    {
+        //require model file
+        require_once '../app/models/' . $model . '.php';
+
+        //instantiate model
+        return new $model();
+    }
+    //load view
+    public function view($view, $data = [])
+    {
+        //check for view file
+        if (file_exists('../app/views/' . $view . '.php')) {
+            require_once '../app/views/' . $view . '.php';
+        } else {
+            //view does not exist
+            die('View does not exist');
+        }
+    }
+
+}
---
+++ app/views/pages/index.php
@@ -0,0 +1,5 @@
+<?php
+require APPROOT. '/views/inc/header.php';
+       echo '<h1>'.$data['title'].'</h1>';
+       echo '<span>'.$data['description'].'</span>';
+require APPROOT. '/views/inc/footer.php';
