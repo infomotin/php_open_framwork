a696d61e44a7a5c2666d167f3f4e0106f6594994
Abdul Motin <motin.mmk.jr@gmail.com>
2021-11-24 12:47:56 +0600

    Header and Footer

---
+++ app/views/inc/footer.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="<?php echo URLROOT;?>/js/main.js">
+</body>
+</html>
--- app/views/index.php
+++ app/views/index.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+<?php
+require APPROOT. '/views/inc/header.php';
         echo '<H1>'.$data['title'].'</H1>'; 
         echo '<H2>'.$data['description'].'</H2>';
         // echo APPROOT;
-    ?>
-    </body>
-</html>
+require APPROOT. '/views/inc/footer.php';
