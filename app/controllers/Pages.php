6573968f59e47b4cfe0af56162556320142c0fef
Abdul Motin <motin.mmk.jr@gmail.com>
2021-11-24 11:46:35 +0600

    Chang Core.php file

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,11 +1,14 @@
 <?php
 
-class Pages{
+class Pages extends Controller {
     public function __construct(){
         
     }
     public function index(){
-        echo 'Hello from the index method';
+        // i extended the base controller class and i user parrent class constructor
+        // to load the view
+        $this->view('index');
+        
     }
     public function about($id){
         echo "About page with id: {$id}";
