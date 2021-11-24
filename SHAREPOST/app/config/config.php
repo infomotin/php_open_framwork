8638dccf0bb45e8e7acb7efb2530785db144a375
Abdul Motin <motin.mmk.jr@gmail.com>
2021-11-24 16:18:56 +0600

    create APPLICATION WITH THIS FRAMWORK

---
+++ SHAREPOST/app/config/config.php
@@ -0,0 +1,21 @@
+<?php 
+//DB params 
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', 'root');
+// define('DB_NAME', 'buildphp');
+
+// DB params 
+define('DB_HOST', 'localhost');
+define('DB_USER', '__YOUR_USER_NAME__');
+define('DB_PASS', '__YOURPASSWORD__');
+define('DB_NAME', '__YOURDB__');
+
+
+//get app base dir file 
+//app root
+define('APPROOT',dirname(dirname(__FILE__)));
+//url root
+define('URLROOT','http://localhost:8088/BuildPhpFramwork');
+//site name
+define('SITENAME','__YOUR_fRAMWORK__');
